466e8cd8de112869ae57679ebebdc60e1d6e3ed6
yossef ehab <yosseffehabb@gmail.com>
2025-11-16 19:54:10 +0200

    first commit

---
+++ components/Navbar.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+function Navbar() {
+  return (
+    <header>
+      <nav>
+        <Link href="/" className="logo">
+          <Image src="/icons/logo.png" alt="logo" width={24} height={24} />
+          <p>DevEvent</p>
+        </Link>
+
+        <ul>
+          <Link href="/">Home</Link>
+          <Link href="/">Events</Link>
+          <Link href="/">Create Events</Link>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Navbar;
